619bd573e2531f96f566d0d0ffb36f65f33d562a
Alibek <alibekkavkaz69@gmail.com>
2026-01-27 13:37:43 +0500

    Update data_types.py file

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,19 @@
+# Example 1: integer type
+age = 19
+print(type(age))
+
+# Example 2: float type
+temperature = 36.6
+print(type(temperature))
+
+# Example 3: string type
+country = "Kazakhstan"
+print(type(country))
+
+# Example 4: boolean type
+is_student = True
+print(type(is_student))
+
+# Example 5: list type
+subjects = ["Calculus", "Linear Algebra", "PP2"]
+print(type(subjects))
