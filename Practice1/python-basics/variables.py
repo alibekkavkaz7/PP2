4243b36e36e1432ce79c8987a323a1e93772dd75
Alibek <alibekkavkaz69@gmail.com>
2026-01-27 13:28:33 +0500

    Update variables examples

---
+++ Practice1/python-basics/variables.py
@@ -0,0 +1,24 @@
+# Example 1: variable with my name 
+name = "Alibek"
+print(name)
+
+# Example 2: sum numbers 
+apples = 7
+oranges = 4
+print(apples + oranges)
+
+# Example 3: updating a variable
+balance = 500
+balance += 200
+print(balance)
+
+# Example 4: different variables in one print
+city = "Almaty" 
+year = 2026
+print(city, year)
+
+# Example 5: variable used in expression
+length = 8
+width = 5
+area = length * width
+print(area)
