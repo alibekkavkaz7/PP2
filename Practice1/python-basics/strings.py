c28d85e17e5891bddbbb49baf6099ceabd2800c1
Alibek <alibekkavkaz69@gmail.com>
2026-01-27 13:49:21 +0500

    update string.py file

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,21 @@
+# Example 1: simple string
+text = "I study at KBTU"
+print(text)
+
+# Example 2: string length
+name = "Alibek"
+print(len(name))
+
+# Example 3: uppercase and lowercase
+city = "Almaty"
+print(city.upper())
+print(city.lower())
+
+# Example 4: string concatenation
+uni = "KBTU"
+subject = " Programming"
+print(uni + subject)
+
+# Example 5: string slicing
+word = "University"
+print(word[0:4])
