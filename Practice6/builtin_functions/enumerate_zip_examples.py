aabd7b601fa33378a7d487cf66f3229ba0ebf49e
Alibek <alibekkavkaz69@gmail.com>
2026-03-17 20:21:09 +0500

    update enumerate and zip

---
+++ Practice6/builtin_functions/enumerate_zip_examples.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+
+for i, v in enumerate(fruits):
+    print(i, v)
+
+names = ["Ali", "John", "Sara"]
+scores = [85, 90, 95]
+
+for n, s in zip(names, scores):
+    print(n, s)
